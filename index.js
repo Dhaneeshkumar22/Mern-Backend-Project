29d97f75c2ef42fb119f752a8ee7de75c1d300ea
Dhaneeshkumar22 <130636939+Dhaneeshkumar22@users.noreply.github.com>
2025-09-17 15:35:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const connectDB = require("./config/db")
 
-const useRoute=require("./Routes/userroutes")
+const useRoute=require("./routes/userRoutes")
 require("dotenv").config();
 const cors=require("cors")
 const app=express()
